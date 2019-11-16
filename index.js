b64221921c27739d0446ccd24968edb09377c07d
Craig Donavin <crdonavin@gmail.com>
2019-11-16 03:03:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(name, email) {    
+  return  fetch("http://localhost:3000/users", configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+      })
+      .then(function(response){
+        return response.json()
+      })
+
+}
